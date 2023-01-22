6b9e2bd00f433d6cdc246106114bc231cdc61140
treva556 <marktreva92@gmail.com>
2023-01-23 01:29:56 +0300

    edited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
-//import { ReactDOM } from "react";
-//import { BrowserRouter as Router } from "react-router-dom";
-//import App from "../App";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
